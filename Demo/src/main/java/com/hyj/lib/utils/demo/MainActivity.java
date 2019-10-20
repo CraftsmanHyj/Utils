eb350416d91d49e732b1e4535dc9f6dbaa8e6677
AsyncHyj <it21sj@foxmail.com>
2019-10-20 09:21:06 +0800

    立项

---
+++ Demo/src/main/java/com/hyj/lib/utils/demo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.hyj.lib.utils.demo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
